737a6879086c6bf42fc77421a7a734e48d9cd6a3
enigmanx20 <enigmanx20@gmail.com>
2023-08-17 15:35:35 +0900

    20230817

--- train_utils.py
+++ train_utils.py
@@ -192,7 +192,7 @@ def prepare_aug(config):
                                 transforms.RandomHorizontalFlip(p=0.5),
                                 transforms.RandomVerticalFlip(p=0.5), 
                                 transforms.ToTensor(),
-                                transforms.Normalize(*config.image_stat)  ])
+                                transforms.Normalize(*config.image_stats)  ])
         else:
             tra = transforms.Compose([ transforms.RandomResizedCrop(config['image_size'], scale=(0.8, 1.), ratio=(0.75, 1.3333333333333333)),
                                 transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1) ], p=0.8), # cf https://arxiv.org/pdf/1703.02442.pdf
@@ -201,24 +201,24 @@ def prepare_aug(config):
                                 transforms.RandomHorizontalFlip(p=0.5),
                                 transforms.RandomVerticalFlip(p=0.5), 
                                 transforms.ToTensor(),
-                                transforms.Normalize(*config.image_stat)  ])
+                                transforms.Normalize(*config.image_stats)  ])
 
         tra_val = transforms.Compose([  transforms.CenterCrop(287),                # equivalent to 20x (0.5mpp)
                                         transforms.Resize(config['vaal_image_size']),
                                         transforms.ToTensor(),
-                                        transforms.Normalize(*config.image_stat)  ])
+                                        transforms.Normalize(*config.image_stats)  ])
 
     elif 'segPANDA' in  os.path.basename(config["dataset_dir"]):
         tra = transforms.Compose([     
                                    transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1) ], p=0.8), # cf https://arxiv.org/pdf/1703.02442.pdf
                                    transforms.RandomApply([utils.GaussianBlur([.1, 2.])], p=0.5),
                                    transforms.ToTensor(),
-                                   transforms.Normalize(*config.image_stat) ])
+                                   transforms.Normalize(*config.image_stats) ])
 
         tra_val = transforms.Compose([  
                                         transforms.CenterCrop(config['val_image_size']), 
                                         transforms.ToTensor(),
-                                        transforms.Normalize(*config.image_stat) ])
+                                        transforms.Normalize(*config.image_stats) ])
         
         tra_kornia = [
                         kornia.augmentation.RandomCrop((config['image_size'], config['image_size'])),
@@ -234,12 +234,12 @@ def prepare_aug(config):
                                    transforms.RandomHorizontalFlip(p=0.5),
                                    transforms.RandomVerticalFlip(p=0.5), 
                                    transforms.ToTensor(),
-                                   transforms.Normalize(*config.image_stat)  ])
+                                   transforms.Normalize(*config.image_stats)  ])
 
         tra_val = transforms.Compose([  transforms.CenterCrop(287),                # equivalent to 20x (0.5mpp)
                                         transforms.Resize(config['vaal_image_size']),
                                         transforms.ToTensor(),
-                                        transforms.Normalize(*config.image_stat)  ])
+                                        transforms.Normalize(*config.image_stats)  ])
 
     elif 'PCam' in  os.path.basename(config["dataset_dir"]):
         tra = transforms.Compose([ transforms.RandomResizedCrop(config['image_size'], scale=(0.8, 1.), ratio=(0.75, 1.3333333333333333)),
@@ -249,12 +249,12 @@ def prepare_aug(config):
                                    transforms.RandomHorizontalFlip(p=0.5),
                                    transforms.RandomVerticalFlip(p=0.5), 
                                    transforms.ToTensor(),
-                                   transforms.Normalize(*config.image_stat)  ])
+                                   transforms.Normalize(*config.image_stats)  ])
 
         tra_val = transforms.Compose([     #transforms.CenterCrop(287),                # equivalent to 20x (0.5mpp)
                                         transforms.Resize(config['vaal_image_size']),
                                         transforms.ToTensor(),
-                                        transforms.Normalize(*config.image_stat)  ])
+                                        transforms.Normalize(*config.image_stats)  ])
         tra_kornia = None
 
     else:
